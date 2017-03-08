6ba791080b03c45bb7d33ed51b0fe9e25f372b9d
Vit S <fornit1917@gmail.com>
2017-03-08 12:44:04 +0300

    topbar

--- js/components/top-bar.js
+++ js/components/top-bar.js
@@ -9,6 +9,10 @@ export default class TopBar extends React.Component {
                         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
                     </svg>
                 </div>
+                <div className="app-header">
+                    <div className="app-logo"></div>
+                    <div className="app-header-text">Music Shelf Radio</div>
+                </div>
             </div>
         )
     }
